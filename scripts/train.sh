bbc11a562923089973096afb0a827e30ea002250
Ada4321 <513220032@qq.com>
2023-07-17 23:53:40 +0800

    update config

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
-nohup python /home/zhuhe/HPE-with-Diffusion/main.py \
+nohup python /home/zhuhe/Improve-HPE-with-Diffusion/main.py \
 --phase 'train' \
---gpu_ids 0 \
---config /home/zhuhe/HPE-with-Diffusion/config/res_diff_plus.json >/home/zhuhe/HPE-with-Diffusion/logs/train_resdiffplus.log 2>& 1&
+--gpu_ids 1 \
+--config /home/zhuhe/Improve-HPE-with-Diffusion/config/sanitycheck.json >/home/zhuhe/Improve-HPE-with-Diffusion/logs/train_sanitycheck.log 2>& 1&
