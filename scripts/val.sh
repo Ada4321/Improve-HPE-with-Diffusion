166e62e96346ee12b47c0b74aab1986cade037db
“Ada4321” <513220032@qq.com>
2023-08-23 18:11:32 +0800

    add visualization

--- scripts/val.sh
+++ scripts/val.sh
@@ -2,4 +2,4 @@ export CUDA_LAUNCH_BLOCKING=1
 nohup python /root/Improve-HPE-with-Diffusion/main.py \
 --phase 'val' \
 --gpu_ids 1 \
---config /root/Improve-HPE-with-Diffusion/config/sanitycheck_val.json >/root/Improve-HPE-with-Diffusion/logs/val_rleckpt.log 2>& 1&
+--config /root/Improve-HPE-with-Diffusion/config/fixed_res_and_diff_val.json >/root/Improve-HPE-with-Diffusion/logs/val_fixed_clip.log 2>& 1&
